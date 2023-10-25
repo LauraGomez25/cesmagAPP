8bbfcc50367f08cf570e7bc9e3c8b304cc1e2495
Laura <lsgomez.5747@unicesmag.edu.co>
2023-10-25 08:10:47 -0500

    Revert "Tercera version"
    This reverts commit 3489c4f91d493b8ba24ebe68fdae1b72afe91f37.

--- index.php
+++ index.php
@@ -9,9 +9,5 @@
 <body>
     <h1>Cesmag App</h1>
     <h2>Laura Gomez</h2>
-
-    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit.
-        Quam minima velit vero deleniti recusandae dolores odio minus quos eius at,
-        consequuntur quibusdam veritatis consequatur possimus reprehenderit sit, soluta ullam in.</p>
 </body>
 </html>
