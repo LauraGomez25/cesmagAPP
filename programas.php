4ae70a1daaa6bda446955fbd34abc6493b7afbc8
Laura <lsgomez.5747@unicesmag.edu.co>
2023-10-25 09:11:29 -0500

    Tabla Programas

--- programas.php
+++ programas.php
@@ -1,3 +1,27 @@
 <?php
 include 'index.php'
 ?>
+
+<h1>Tabla Programas</h1>
+<table class="table">
+  <thead>
+    <tr>
+      <th scope="col">Codigo</th>
+      <th scope="col">Nombre</th>
+      <th scope="col">Facultad</th>    
+      <th scope="col">Opciones</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <td>100</td>
+      <td>Color</td>
+      <td>10</td>
+      <td>
+        <button type="button" class="btn btn-success">EDITAR</button>
+        <button type="button" class="btn btn-danger">ELIMINAR</button>
+      </td>
+    </tr>
+
+  </tbody>
+</table>
