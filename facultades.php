32518ddb1531c24c70d192b54d6e4173cfea3042
Laura <lsgomez.5747@unicesmag.edu.co>
2023-10-25 09:06:47 -0500

    Tabla Facultades

---
+++ facultades.php
@@ -0,0 +1,25 @@
+<?php
+include 'index.php'
+?>
+
+<h1>Tabla Facultades</h1>
+<table class="table">
+  <thead>
+    <tr>
+      <th scope="col">Codigo</th>
+      <th scope="col">Nombre</th>
+      <th scope="col">Opciones</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <td>10</td>
+      <td>Artes</td>
+      <td>
+        <button type="button" class="btn btn-success">EDITAR</button>
+        <button type="button" class="btn btn-danger">ELIMINAR</button>
+      </td>
+    </tr>
+
+  </tbody>
+</table>
